d2890b77d12cc280a24196677af6bc0a72aaa1ca
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-04-23 23:14:17 +0700

    Feat: Create main class

---
+++ DecolistApp/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        // SwingUtilities.invokeLater(() -> { // frame = new Task(); });
+    }
+}
